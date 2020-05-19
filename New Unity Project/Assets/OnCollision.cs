780c49d77cbc2083e2b5bbb03b003115764c4dcd
JamesHRMIT <s3803084@student.rmit.edu.au>
2020-05-19 12:54:50 +1000

    Initial Commit with Gitignore
    After working on the project, uploaded to github for collaborators

---
+++ New Unity Project/Assets/OnCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnCollision : MonoBehaviour
+{
+    public Collision collision;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+  
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
---
+++ New Unity Project/Assets/OutofBounds.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OutofBounds : MonoBehaviour
+{
+    public float outOfMap = -10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if{ gameObject.transform.position.y <= outOfMap}
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(SceneManager.GetSceneByName("SampleScene"));
+            
+        }
+    }
+}
